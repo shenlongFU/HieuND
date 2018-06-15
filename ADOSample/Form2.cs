2ba1155239b74d0d63e66b72b2c7fad49027a39f
Shere Khan <Shere Khan@DESKTOP-742HPCB>
2018-06-15 14:57:03 +0700

    Add project files.

---
+++ ADOSample/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADOSample
+{
+    public partial class Form2 : Form
+    {
+        string ConnectionString =
+            ConfigurationManager.ConnectionStrings["EmployeeDB"].
+            ConnectionString;
+        DataView dv;
+        public Form2()
+        {
+            InitializeComponent();
+            //get ProjectMember and Employee
+            string select = "select * from ProjectMember; select * from Employee";
+            SqlDataAdapter da = new SqlDataAdapter(select, ConnectionString);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            //bind employee to dropdownlist
+            DataTable employee = ds.Tables[1];
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "id";
+            comboBox1.DataSource = employee;
+            //bind projectmember to gridview
+            dv = new DataView(ds.Tables[0]);
+            dataGridView1.DataSource = dv;
+            //valueChanged event for dropdownlist
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            ComboBox1_SelectedIndexChanged(null, null);
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //get selected employee id
+            int id = int.Parse(comboBox1.SelectedValue.ToString());
+            dv.RowFilter = "employeeID = " + id;
+        }
+    }
+}
---
+++ ADOSample/Form3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADOSample
+{
+    public partial class Form3 : Form
+    {
+        string ConnectionString =
+            ConfigurationManager.ConnectionStrings["EmployeeDB"].
+            ConnectionString;
+        SqlDataAdapter da;
+        DataTable dt;
+        public Form3()
+        {
+            InitializeComponent();
+            string select = "select * from employee";
+            da = new SqlDataAdapter(select, ConnectionString);
+            dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            //make id column is a PK
+            dt.PrimaryKey = new DataColumn[] { dt.Columns[0] };
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //output selected row to textbox
+            int i = dataGridView1.CurrentRow.Index; ;
+            if(i >= 0)
+            {
+                string id = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                string name = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                textBox1.Text = id; textBox2.Text = name;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //assuming that textbox1,2 is not empty
+            string id = textBox1.Text;
+            string name = textBox2.Text;
+            DataRow dr = dt.Rows.Find(id);
+            if(dr != null)
+            {
+                MessageBox.Show("Employee ID cannot be duplicated"); return;
+            }
+            //make a new row and add to dt
+            dr = dt.NewRow();
+            dr[0] = id;
+            dr[1] = name;
+            dt.Rows.Add(dr);
+            SqlCommandBuilder sb = new SqlCommandBuilder(da);
+            da.Update(dt);//update dt to datasource
+            MessageBox.Show("new employee has been added");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            string name = textBox2.Text;
+            //find id
+            DataRow dr = dt.Rows.Find(id);
+            dr[1] = name; //update name only
+            string update = "update employee set name= @name where id = @id";
+            SqlCommand updateCmd = new SqlCommand();
+            updateCmd.CommandText = update;
+            updateCmd.Parameters.AddWithValue("id", id);
+            updateCmd.Parameters.AddWithValue("name", name);
+            updateCmd.Connection = new SqlConnection(ConnectionString);
+            da.UpdateCommand = updateCmd;
+            da.Update(dt); //update to data source
+            MessageBox.Show("Empoyee is updated");
+        }
+    }
+}
